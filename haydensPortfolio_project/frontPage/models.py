17bcdeb8a117fefb7ccee0c36cf21d75605b3343
martin <mas6553@psu.edu>
2015-07-06 21:09:17 -0400

    small issue
    If the images aren't the same size, the portfolio ends up looking kind
    of dumb

--- haydensPortfolio_project/frontPage/models.py
+++ haydensPortfolio_project/frontPage/models.py
@@ -5,6 +5,7 @@ class PortfolioPiece(models.Model):
     #id = models.IntegerField(unique=True)
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=256)
+    # if I change this to /static instead, I can get rid of the / in front of the url that is in the index
     image = models.ImageField(upload_to='static/img/haydensPortfolioPieces', verbose_name='My Photo', null=True, blank=False)
 
     def __unicode__(self):  #For Python 2, use __str__ on Python 3
